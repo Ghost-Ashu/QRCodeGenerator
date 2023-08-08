5c94dcbc5e6506d29b267789107f84f44506ba5e
Ashutosh <41056892+Ghost-Ashu@users.noreply.github.com>
2023-08-08 17:55:55 +0530

    gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+bind = '0.0.0.0:8000'  # Change the port and host as needed
+workers = 4  # Number of worker processes
+
+# If you're using a virtual environment, specify its path
+# pythonpath = '/path/to/your/virtual/environment'
